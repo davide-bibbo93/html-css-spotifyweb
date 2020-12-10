3e05c2084adce1a9deef7879b566da4e92e3872d
Davide <bibbodavide@gmail.com>
2020-12-10 16:47:37 +0100

    aggiunto elementi script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+prompt('Benvenuto in SpotifyWeb! Inserire qui il tuo nome.');
+alert('Ciao Davide! :)');
+document.getElementById("upgrade").innerText= 'Ciao Davide, Bentornato!';
+document.getElementById("name").innerText= 'Davide Bibbò';
+document.getElementById("create").innerText= 'Creato per Te';
